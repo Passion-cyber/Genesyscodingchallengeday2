9c59796202d2087fde54b3ee85a09a8dee013b33
Ibelachi Chinedu Franklyn <chineduibelachi@gmail.com>
2022-10-08 15:43:20 +0100

    initial commit

---
+++ sript.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal();
+ sr.reveal('.right-card-section', {
+    duration: 2000,
+    origin: "bottom"
+ });
+
+ sr.reveal('.left-card-section', {
+    duration: 2000,
+    origin: "bottom",
+    distance: "300px"
+ });
+
+ k/lx
